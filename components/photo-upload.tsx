76772ec3da59294511318851cc0b2cf3473d242f
Rio Edwards <rioredwards@gmail.com>
2025-01-04 22:54:44 -0800

    About to clean up photo upload component

--- components/photo-upload.tsx
+++ components/photo-upload.tsx
@@ -1,38 +1,38 @@
-
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { useToast } from '@/hooks/use-toast';
-import Image from 'next/image';
-import { useRef, useState } from 'react';
-import PulseLoader from 'react-spinners/PulseLoader';
-import { uploadPhoto } from '../app/actions';
-import { useCookie } from '../context/CookieCtx';
-import { reduceFileSize } from '../lib/imgCompress';
-import { Photo } from '../lib/types';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useToast } from "@/hooks/use-toast";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
+import { uploadPhoto } from "../app/actions";
+import { useCookie } from "../context/CookieCtx";
+import { reduceFileSize } from "../lib/imgCompress";
+import { Photo } from "../lib/types";
+import { RotatingGradientBorder } from "./ui/RotatingGradientBorder";
 
 interface Props {
-  addPhoto: (photo: Photo) => void
+  addPhoto: (photo: Photo) => void;
 }
 
 export function PhotoUpload({ addPhoto }: Props) {
-  const [file, setFile] = useState<File | null>(null)
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const { toast } = useToast()
-  const fileInputRef = useRef<HTMLInputElement>(null)
-  const { cookie } = useCookie()
+  const [file, setFile] = useState<File | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { toast } = useToast();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { cookie } = useCookie();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsSubmitting(true)
+    setIsSubmitting(true);
 
     if (!cookie) {
       toast({
         title: "Error",
         description: "You must enable cookies to upload a photo.",
         variant: "destructive",
-      })
-      setIsSubmitting(false)
+      });
+      setIsSubmitting(false);
       return;
     }
 
@@ -41,12 +41,12 @@ export function PhotoUpload({ addPhoto }: Props) {
         title: "Error",
         description: "Please select a photo to upload.",
         variant: "destructive",
-      })
-      setIsSubmitting(false)
+      });
+      setIsSubmitting(false);
       return;
     }
 
-    const formData = new FormData()
+    const formData = new FormData();
 
     // If file size > 500kB, resize such that width <= 1000, quality = 0.9
     const MAX_FILE_SIZE = 500 * 1000; // 500kB
@@ -56,83 +56,110 @@ export function PhotoUpload({ addPhoto }: Props) {
 
     const resizedImg = await reduceFileSize(file, MAX_FILE_SIZE, MAX_WIDTH, MAX_HEIGHT, QUALITY);
 
-    formData.append('file', resizedImg)
+    formData.append("file", resizedImg);
 
-    const res = await uploadPhoto(formData)
+    const res = await uploadPhoto(formData);
     if (res.error || !res.data) {
-      console.error(res.error)
+      console.error(res.error);
       toast({
         title: "Error",
         description: "There was a problem uploading your photo.",
         variant: "destructive",
-      })
-      setIsSubmitting(false)
+      });
+      setIsSubmitting(false);
       return;
     }
 
     toast({
       title: "Success",
       description: "Your photo has been uploaded.",
-    })
-    setFile(null)
-    setIsSubmitting(false)
-    addPhoto(res.data)
-  }
+    });
+    setFile(null);
+    setIsSubmitting(false);
+    addPhoto(res.data);
+  };
 
   const handleCancel = () => {
-    setFile(null)
+    setFile(null);
     if (fileInputRef.current) {
-      fileInputRef.current.value = ''
+      fileInputRef.current.value = "";
     }
-  }
+  };
 
   return (
-    <section className='flex flex-col items-center justify-center mb-8'>
-      <form onSubmit={handleSubmit} className={"rounded-2xl" + (file ? " gradient-card-wrapper w-[400px] h-[400px] hover:shadow-sm" : "")}>
-        <div className={'flex flex-col justify-center items-center h-full w-full' + (file ? " gradient-card-content rounded-2xl bg-gray-50 border border-gray-200 hover:bg-gray-100 hover:border-gray-300 px-12" : "")}>
-          {!file && <div className='gradient-btn-wrapper rounded-full w-[300px] h-[75px]'>
-            <Label htmlFor="photo" className={(file ? "hidden" : "") + " gradient-btn-content rounded-full  border border-gray-200  hover:border-gray-300 cursor-pointer text-center flex items-center justify-center font-bold transition"}>
-              Upload Your Dog <span className='text-2xl ml-2'> 🐶</span> </Label>
-          </div>}
-          {file &&
-            <>
-              <p className="text-center mb-2 font-bold text-lg">Upload this Dog?</p>
-              <div className='h-[1px] w-full bg-gradient-to-r from-transparent via-gray-200 to-transparent mb-6' />
-              <Label htmlFor="photo"
-                className="w-48 h-48 mb-8 relative aspect-square overflow-hidden rounded-lg cursor-pointer"
-              ><Image
-                  src={URL.createObjectURL(file)}
-                  alt="Dog photo"
-                  width={0}
-                  height={0}
-                  fill={true}
-                  className="object-cover"
-                />
-              </Label></>}
-          <Input
-            id="photo"
-            type="file"
-            accept="image/*"
-            onChange={(e) => setFile(e.target.files?.[0] || null)}
-            className="hidden"
-            ref={fileInputRef}
-          />
-          {file && <div className="flex space-x-2 w-full">
-            <Button onClick={handleCancel} variant="outline" className='bg-gray-200 hover:bg-gray-300 flex-1 transition'>
-              Cancel
-            </Button>
-            <Button type="submit"
-              disabled={isSubmitting}
-              variant="default" className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium flex-1 transition'>
-              {!isSubmitting ? "Upload" : <>
-                <PulseLoader color="white" loading={true} size={5} />
-              </>}
-            </Button>
-            {/* <button type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Purple to Pink</button> */}
-          </div>}
-        </div>
-      </form>
+    <section className="flex flex-col items-center justify-center mb-8">
+      <RotatingGradientBorder className="rounded-2xl">
+        <form onSubmit={handleSubmit} className={"rounded-2xl" + (file ? " hover:shadow-sm" : "")}>
+          <div
+            className={
+              "flex flex-col justify-center items-center" +
+              (file
+                ? " rounded-2xl bg-gray-50 border border-gray-200 hover:bg-gray-100 hover:border-gray-300 px-12"
+                : "")
+            }>
+            {!file && (
+              <Label
+                htmlFor="photo"
+                className={
+                  (file ? "hidden" : "") +
+                  "  rounded-full  border border-gray-200  hover:border-gray-300 cursor-pointer text-center flex items-center justify-center font-bold transition"
+                }>
+                Upload Your Dog <span className="text-2xl ml-2"> 🐶</span>{" "}
+              </Label>
+            )}
+            {file && (
+              <>
+                <p className="text-center mb-2 font-bold text-lg">Upload this Dog?</p>
+                <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-gray-200 to-transparent mb-6" />
+                <Label
+                  htmlFor="photo"
+                  className="w-48 h-48 mb-8 relative aspect-square overflow-hidden rounded-lg cursor-pointer">
+                  <Image
+                    src={URL.createObjectURL(file)}
+                    alt="Dog photo"
+                    width={0}
+                    height={0}
+                    fill={true}
+                    className="object-cover"
+                  />
+                </Label>
+              </>
+            )}
+            <Input
+              id="photo"
+              type="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files?.[0] || null)}
+              className="hidden"
+              ref={fileInputRef}
+            />
+            {file && (
+              <div className="flex space-x-2 w-full">
+                <Button
+                  onClick={handleCancel}
+                  variant="outline"
+                  className="bg-gray-200 hover:bg-gray-300 flex-1 transition">
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  variant="default"
+                  className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium flex-1 transition">
+                  {!isSubmitting ? (
+                    "Upload"
+                  ) : (
+                    <>
+                      <PulseLoader color="white" loading={true} size={5} />
+                    </>
+                  )}
+                </Button>
+                {/* <button type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Purple to Pink</button> */}
+              </div>
+            )}
+          </div>
+        </form>
+      </RotatingGradientBorder>
     </section>
-  )
+  );
 }
-
--- components/ui/RotatingGradientBorder.tsx
+++ components/ui/RotatingGradientBorder.tsx
@@ -2,6 +2,93 @@ import 'culori/css';
 import { formatCss, useMode as getMode, modeOklch } from 'culori/fn';
 import * as React from "react";
 
+interface Props {
+  border?: boolean
+  borderColors?: string[]
+  borderOpacity?: number
+  borderCover?: number
+  borderSkew?: number
+  borderSoftEdge?: number
+  borderWidth?: number
+  shadow?: boolean
+  shadowSkew?: number
+  shadowWidth?: number
+  shadowOpacity?: number
+  shadowBlur?: number
+  shadowSoftEdge?: number
+  backgroundColor?: string
+  spinAnimation?: boolean
+  spinAnimationSpeed?: number
+  className?: string
+}
+
+const RotatingGradientBorder = React.forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<Props>
+>(({
+  border = true,
+  borderColors = ["red", "orange", "blue"],
+  borderOpacity = .6,
+  borderCover = 100,
+  borderWidth = 1,
+  borderSkew = 0,
+  borderSoftEdge = .01,
+  shadow = true,
+  shadowSkew = 0,
+  shadowOpacity = 0.4,
+  shadowWidth = 3,
+  shadowBlur = 6,
+  shadowSoftEdge = .01,
+  backgroundColor = "white",
+  spinAnimation = true,
+  spinAnimationSpeed = 3,
+  className,
+  children,
+}, ref) => {
+  const oklchBorderColors = borderColors.map(convertToOklch);
+
+  const borderBgStyle = createBgStyle(oklchBorderColors, borderCover, borderSkew, borderSoftEdge);
+  const shadowBgStyle = createBgStyle(oklchBorderColors, borderCover, shadowSkew, shadowSoftEdge);
+
+  const sharedStyle: React.CSSProperties = {
+    animation: spinAnimation ? `spin ${spinAnimationSpeed}s infinite linear` : "none",
+    position: "absolute",
+    zIndex: -1,
+  };
+
+  const borderStyle: React.CSSProperties = {
+    ...sharedStyle,
+    opacity: borderOpacity,
+    background: borderBgStyle,
+    inset: `-${borderWidth}px`,
+  };
+
+  const shadowStyle: React.CSSProperties = {
+    ...sharedStyle,
+    opacity: shadowOpacity,
+    background: shadowBgStyle,
+    inset: `-${shadowWidth}px`,
+    filter: `blur(${shadowBlur}px)`,
+    WebkitFilter: `blur(${shadowBlur}px)`,
+  };
+
+  return (
+    <>
+      {/* Parent */}
+      <div style={{ position: "relative" }} ref={ref}>
+        {/* Inner Content (passed in through children/props) */}
+        <div className={className} style={{ backgroundColor }}>{children}</div>
+        {/* Gradient Border */}
+        {border && <div className={className} style={borderStyle} />}
+        {/* Gradient Shadow */}
+        {shadow && <div className={className} style={shadowStyle} />}
+      </div >
+    </>
+  )
+})
+
+RotatingGradientBorder.displayName = "RotatingGradientBorder"
+
 function convertToOklch(color: string): string {
   const oklch = getMode(modeOklch);
   const oklchColor = oklch(color);
@@ -23,12 +110,12 @@ function createTransparencyStrings(coverage: number, skew: number = 0, softEdge:
   const start = middle - (coverage / 2);
   const end = start + coverage;
   // Rotate
-  const adjustedStart = start + ((skew * coverage) / 2);
-  const adjustedEnd = end + ((skew * coverage) / 2);
+  const rotatedStart = start + ((skew * coverage) / 2);
+  const rotatedEnd = end + ((skew * coverage) / 2);
   // Expand
   // If skew pushes a val past 1 (end) or 0 (start), then clamp it and add the remainder to the other value
-  const expandedStart = adjustedEnd > 1 ? adjustedStart - (adjustedEnd % 1) : adjustedStart;
-  const expandedEnd = adjustedStart < 0 ? adjustedEnd + Math.abs(adjustedStart % 1) : adjustedEnd;
+  const expandedStart = rotatedEnd > 1 ? rotatedStart - (rotatedEnd % 1) : rotatedStart;
+  const expandedEnd = rotatedStart < 0 ? rotatedEnd + Math.abs(rotatedStart % 1) : rotatedEnd;
   // Clamp
   const clampedStart = Math.max(expandedStart, 0);
   const clampedEnd = Math.min(expandedEnd, 1);
@@ -56,91 +143,5 @@ function createBgStyle(borderColors: string[], borderCover: number, skew: number
   const bgStyle = (colorString: string) => `conic-gradient(from calc(var(--bg-angle) - ${skew}deg) in oklch, ${colorString}) border-box`;
   return bgStyle(borderBgColorString);
 }
-
-interface Props {
-  border?: boolean
-  borderColors?: string[]
-  borderOpacity?: number
-  borderCover?: number
-  borderSkew?: number
-  borderSoftEdge?: number
-  borderWidth?: number
-  shadow?: boolean
-  shadowSkew?: number
-  shadowWidth?: number
-  shadowOpacity?: number
-  shadowBlur?: number
-  shadowSoftEdge?: number
-  spinAnimation?: boolean
-  spinAnimationSpeed?: number
-}
-
-const RotatingGradientBorder = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> &
-  Props
->(({
-  border = true,
-  borderColors = ["red", "blue", "red", "blue",],
-  borderOpacity = .6,
-  borderCover = 100,
-  borderWidth = 1,
-  borderSkew = 0,
-  borderSoftEdge = .01,
-  shadow = true,
-  shadowSkew = 0,
-  shadowOpacity = 0.4,
-  shadowWidth = 3,
-  shadowBlur = 6,
-  shadowSoftEdge = .01,
-  spinAnimation = true,
-  spinAnimationSpeed = 3,
-  children,
-  ...props
-}, ref) => {
-  const oklchBorderColors = borderColors.map(convertToOklch);
-
-  const borderBgStyle = createBgStyle(oklchBorderColors, borderCover, borderSkew, borderSoftEdge);
-  const shadowBgStyle = createBgStyle(oklchBorderColors, borderCover, shadowSkew, shadowSoftEdge);
-
-  const sharedStyle: React.CSSProperties = {
-    animation: spinAnimation ? `spin ${spinAnimationSpeed}s infinite linear` : "none",
-    position: "absolute",
-    zIndex: -1,
-    borderRadius: "inherit",
-  };
-
-  const borderStyle: React.CSSProperties = {
-    ...sharedStyle,
-    opacity: borderOpacity,
-    background: borderBgStyle,
-    inset: `-${borderWidth}px`,
-  };
-
-  const shadowStyle: React.CSSProperties = {
-    ...sharedStyle,
-    opacity: shadowOpacity,
-    background: shadowBgStyle,
-    inset: `-${shadowWidth}px`,
-    filter: `blur(${shadowBlur}px)`,
-    WebkitFilter: `blur(${shadowBlur}px)`,
-  };
-
-  return (
-    <>
-      {/* Parent */}
-      <div {...props} style={{ position: "relative" }} ref={ref}>
-        {/* Inner Content (passed in through children/props) */}
-        {children}
-        {/* Gradient Border */}
-        {border && <div style={borderStyle} />}
-        {/* Gradient Shadow */}
-        {shadow && <div style={shadowStyle} />}
-      </div >
-    </>
-  )
-})
-
-RotatingGradientBorder.displayName = "RotatingGradientBorder"
 
 export { RotatingGradientBorder };
